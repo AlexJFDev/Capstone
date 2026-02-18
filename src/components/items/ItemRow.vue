fc5d953aab1408b9adcb63d08f8a65a79362cd1f
alexjfdev <alexjfuhr@gmail.com>
2026-02-18 14:54:48 -0500

    Added ItemRow

---
+++ src/components/items/ItemRow.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { type Item } from '@/testing/dummy-items'
+
+const props = defineProps<{
+  item: Item
+}>()
+</script>
+
+<template>
+  <v-row>
+    {{ item.name }}
+  </v-row>
+</template>
+
+<style scoped>
+
+</style>
--- src/components/workspaces/WorkspaceCard.vue
+++ src/components/workspaces/WorkspaceCard.vue
@@ -1,19 +1,32 @@
 <script setup lang="ts">
 import { type Workspace } from '@/testing/dummy-workspaces'
+import ItemRow from '../items/ItemRow.vue'
+import { items } from '@/testing/dummy-items'
 
 const props = defineProps<{
   workspace: Workspace
 }>()
 
 const { workspace } = props
+
 </script>
 
 <template>
   <v-card
     :title="workspace.name"
-    :text="workspace.description"
     :color="workspace.color"
-  />
+  >
+    <v-card-text>
+      {{ workspace.description }}
+      <v-container>
+        <ItemRow 
+          v-for="itemId in workspace.items"
+          :key="itemId"
+          :item="items[itemId]!"
+        />
+      </v-container>
+    </v-card-text>
+  </v-card>
 </template>
 
 <style scoped>

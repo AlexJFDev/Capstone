31a31f0b3273e15f2b2535bfdcf4e35bf83eec44
alex <alexjfuhr@gmail.com>
2026-02-19 23:16:01 -0500

    Added ItemList

---
+++ src/components/items/ItemList.vue
@@ -0,0 +1,73 @@
+<script setup lang="ts">
+import { items } from '@/testing/dummy-items'
+import ColorSwatch from '@/components/ColorSwatch.vue'
+
+defineProps<{
+  edit: {
+    type: boolean
+    default: false
+  }
+}>()
+
+const model = defineModel<string[]>({ default: [] })
+
+const removeItem = (_itemId: string) => {}
+const addItem = () => {}
+</script>
+
+<template>
+  <div class="item-list bg-white text-black rounded-lg border">
+
+    <v-row
+      v-for="(itemId, index) in model"
+      :key="itemId"
+      class="ma-0"
+      :class="{ 'border-b': edit || index < model.length - 1 }"
+      align="center"
+    >
+      <v-col cols="1" class="d-flex justify-center">
+        <ColorSwatch :color="items[itemId]!.color" />
+      </v-col>
+      <v-col class="pa-0">
+        {{ items[itemId]!.name }}
+      </v-col>
+      <v-col v-if="edit" cols="auto" class="pa-1">
+        <v-btn
+          icon="mdi-trash-can-outline"
+          variant="text"
+          density="compact"
+          size="small"
+          @click="removeItem(itemId)"
+        />
+      </v-col>
+    </v-row>
+
+    <v-row v-if="edit" class="ma-0 add-row" align="center" @click="addItem">
+      <v-col cols="1" class="d-flex justify-center">
+        <div class="add-swatch d-flex align-center justify-center">
+          <v-icon size="8">mdi-plus</v-icon>
+        </div>
+      </v-col>
+      <v-col class="pa-0 text-caption text-medium-emphasis">
+        Add item
+      </v-col>
+    </v-row>
+
+  </div>
+</template>
+
+<style scoped>
+.item-list {
+  overflow: hidden;
+}
+
+.add-row {
+  cursor: pointer;
+}
+
+.add-swatch {
+  width: 10px;
+  height: 10px;
+  border: 1px dashed rgba(0, 0, 0, 0.38);
+}
+</style>
--- src/components/workspaces/WorkspaceCard.vue
+++ src/components/workspaces/WorkspaceCard.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { type Workspace } from '@/testing/dummy-workspaces'
-import ItemRow from '../items/ItemRow.vue'
-import { items } from '@/testing/dummy-items'
+import ItemList from '../items/ItemList.vue'
 
 defineProps<{
   workspace: Workspace
@@ -17,19 +16,10 @@ defineProps<{
   >
     <v-card-text class="d-flex ga-8 flex-column">
       {{ workspace.description }}
-      <v-container class="table-container bg-white text-black rounded-lg border pa-0">
-        <ItemRow
-          v-for="itemId in workspace.items"
-          :key="itemId"
-          :item="items[itemId]!"
-        />
-      </v-container>
+      <ItemList v-model="workspace.items"/>
     </v-card-text>
   </v-card>
 </template>
 
 <style scoped>
-.table-container {
-  overflow: hidden;
-}
 </style>

e92ab9149bb72551687976374f00d5fa6a3ac4ca
alexjfdev <alexjfuhr@gmail.com>
2026-02-22 15:02:29 -0500

    Added RoadmapHeader stub

---
+++ src/components/roadmap/RoadmapHeader.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import type { RoadmapScale } from './types';
+
+const props = defineProps<{
+  itemIds: string[]
+  scale: RoadmapScale
+}>()
+
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>

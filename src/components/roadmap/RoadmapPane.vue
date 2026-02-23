4567c7fd451f49370427094c61eb69987b5f34d3
alexjfdev <alexjfuhr@gmail.com>
2026-02-22 19:15:29 -0500

    Cleaned up CSS

--- src/components/roadmap/RoadmapPane.vue
+++ src/components/roadmap/RoadmapPane.vue
@@ -21,26 +21,24 @@ const scale: RoadmapScale = {
 </script>
 
 <template>
-  <div class="pa-8">
-    <div class="roadmap-pane">
+  <div class="roadmap-pane">
 
-      <!-- Header -->
-      <div class="header">
-        <div class="list-header">
-          <div class="list-box" />
-          <div>Add Item</div>
-        </div>
-        <RoadmapHeader :itemIds="workspace.items" :scale="scale" />
+    <!-- Header -->
+    <div class="header">
+      <div class="list-header">
+        <div class="list-box" />
+        <div>Add Item</div>
       </div>
+      <RoadmapHeader :itemIds="workspace.items" :scale="scale" />
+    </div>
 
-      <!-- Body: Items List & Roadmap Render -->
-      <div class="body">
-        <!-- Item List -->
-        <RoadmapItemList class="item-list" :itemIds="workspace.items" />
+    <!-- Body: Items List & Roadmap Render -->
+    <div class="body">
+      <!-- Item List -->
+      <RoadmapItemList class="item-list" :itemIds="workspace.items" />
 
-        <!-- Roadmap Chart -->
-        <RoadmapChart class="chart" :itemIds="workspace.items" :scale="scale" />
-      </div>
+      <!-- Roadmap Chart -->
+      <RoadmapChart class="chart" :itemIds="workspace.items" :scale="scale" />
     </div>
   </div>
 </template>
@@ -49,8 +47,6 @@ const scale: RoadmapScale = {
 .roadmap-pane {
   max-height: calc(100vh - 64px);
   overflow: auto;
-  display: flex;
-  flex-direction: column;
   border: 2px solid v-bind(PANE_COLOR_PRIMARY);
   border-radius: 4px;
 
@@ -66,13 +62,10 @@ const scale: RoadmapScale = {
     z-index: 1;
 
     .list-header {
-      display: flex;
-      flex-direction: column;
       width: v-bind(LIST_WIDTH_PX);
       border-right: 1px solid v-bind(SECTION_BORDER_COLOR);
       position: sticky;
       left: 0;
-      z-index: 2;
 
       background-color: rgb(var(--v-theme-surface));
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,6 +7,5 @@ const TEST_WORKSPACE_ID = 'w-1a2b3c4d-5e6f-7890-abcd-ef1234567890'
 <template>
   <v-main>
     <RoadmapPane :workspaceId="TEST_WORKSPACE_ID"/>
-    <div style="height: 1000px;"></div>
   </v-main>
 </template>

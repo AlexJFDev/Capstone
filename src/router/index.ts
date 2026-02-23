ea53273b197ab128b76e3fee203ebd27dd42c369
alexjfdev <alexjfuhr@gmail.com>
2026-02-23 00:44:36 -0500

    Made it so test is not included in final builds

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import TestView from '../views/TestView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,11 +9,11 @@ const router = createRouter({
       name: 'home',
       component: HomeView,
     },
-    {
+    ...(import.meta.env.DEV ? [{
       path: '/test',
       name: 'test',
-      component: TestView,
-    },
+      component: () => import('../views/TestView.vue'),
+    }] : []),
   ],
 })
 

3ec3484f8ec8c22c9f9e26005a424b15a2eda949
alexjfdev <alexjfuhr@gmail.com>
2026-02-24 00:40:53 -0500

    Added list expansion

--- src/components/items/ItemList.vue
+++ src/components/items/ItemList.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 import ColorSwatch from '@/components/ColorSwatch.vue'
 import { useItemsStore } from '@/stores/items'
 import { useInterfaceStore } from '@/stores/interface'
@@ -17,12 +17,15 @@ const model = defineModel<string[]>({ default: [] })
 const itemsStore = useItemsStore()
 const interfaceStore = useInterfaceStore()
 
-const visibleItems = computed(() => props.edit ? model.value : model.value.slice(0, props.maxItems))
-const hasMore = computed(() => !props.edit && model.value.length > props.maxItems)
+const expanded = ref(false)
+
+const visibleItems = computed(() => (props.edit || expanded.value) ? model.value : model.value.slice(0, props.maxItems))
+const hasMore = computed(() => !props.edit && !expanded.value && model.value.length > props.maxItems)
 
 const removeItem = (_itemId: string) => {}
 const addItem = () => {}
-const viewMore = () => {}
+const viewMore = () => { expanded.value = true }
+const viewLess = () => { expanded.value = false }
 </script>
 
 <template>
@@ -32,7 +35,7 @@ const viewMore = () => {}
       v-for="(itemId, index) in visibleItems"
       :key="itemId"
       class="ma-0"
-      :class="{ 'border-b': edit || index < visibleItems.length - 1 || hasMore, 'item-row': !edit }"
+      :class="{ 'border-b': edit || index < visibleItems.length - 1 || hasMore || expanded, 'item-row': !edit }"
       align="center"
       @click="!edit && interfaceStore.openItemViewer(itemId)"
     >
@@ -61,6 +64,17 @@ const viewMore = () => {}
       </v-col>
       <v-col class="pa-0 text-caption text-medium-emphasis">
         View {{ model.length - props.maxItems }} more
+      </v-col>
+    </v-row>
+
+    <v-row v-if="expanded" class="ma-0 add-row" align="center" @click="viewLess">
+      <v-col cols="1" class="d-flex justify-center">
+        <div class="add-swatch d-flex align-center justify-center">
+          <v-icon size="8">mdi-chevron-up</v-icon>
+        </div>
+      </v-col>
+      <v-col class="pa-0 text-caption text-medium-emphasis">
+        Show less
       </v-col>
     </v-row>
 

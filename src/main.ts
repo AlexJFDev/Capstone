35c2f6d839b69ffb71ae8e5ab4880d55bb753404
alexjfdev <alexjfuhr@gmail.com>
2026-02-23 23:15:56 -0500

    Added store initialization

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,16 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
+import { useWorkspacesStore } from './stores/workspaces'
+import { useItemsStore } from './stores/items'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(vuetify)
+
+useWorkspacesStore().initializeWorkspaces()
+useItemsStore().initializeItems()
 
 app.mount('#app')

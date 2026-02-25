2e9ee99d857d55bef46f3732f732ae4356bcc29c
alexjfdev <alexjfuhr@gmail.com>
2026-02-25 14:58:59 -0500

    Added close functions

--- src/stores/interface.ts
+++ src/stores/interface.ts
@@ -16,6 +16,9 @@ export const useInterfaceStore = defineStore('panels', () => {
   function openWorkspaces() {
     workspacesOpen.value = true
   }
+  function closeWorkspaces() {
+    workspacesOpen.value = false
+  }
 
   const workspaceEditorOpen = ref(false)
   const editingWorkspaceId = ref<string | undefined>()
@@ -23,12 +26,20 @@ export const useInterfaceStore = defineStore('panels', () => {
     editingWorkspaceId.value = workspaceId
     workspaceEditorOpen.value = true
   }
+  function closeWorkspaceEditor() {
+    editingWorkspaceId.value = undefined
+    workspaceEditorOpen.value = false
+  }
 
   const itemViewerOpen = ref(false)
   const viewingItemId = ref<string | undefined>()
   function openItemViewer(itemId: string) {
     viewingItemId.value = itemId
     itemViewerOpen.value = true
+  }
+  function closeItemViewer() {
+    viewingItemId.value = undefined
+    itemViewerOpen.value = false
   }
 
   const itemEditorOpen = ref(false)
@@ -63,12 +74,15 @@ export const useInterfaceStore = defineStore('panels', () => {
   return {
     workspacesOpen,
     openWorkspaces,
+    closeWorkspaces,
     workspaceEditorOpen,
     editingWorkspaceId,
     openWorkspaceEditor,
+    closeWorkspaceEditor,
     itemViewerOpen,
     viewingItemId,
     openItemViewer,
+    closeItemViewer,
     itemEditorOpen,
     editingItemId,
     openItemEditor,

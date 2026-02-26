80564bc9bad4dcac584c8bb5dddf00448b55e759
alexjfdev <alexjfuhr@gmail.com>
2026-02-26 18:33:38 -0500

    Added route guard against invalid workspaces

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import WorkspacesView from '../views/WorkspacesView.vue'
+import { useWorkspacesStore } from '@/stores/workspaces'
+import { isValidWorkspaceKey } from '@/types'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +22,16 @@ const router = createRouter({
       component: () => import('../views/TestView.vue'),
     }] : []),
   ],
+})
+
+router.beforeEach((to) => {
+  if (to.name === 'workspace') {
+    const workspaceId = to.params.workspaceId as string
+    const workspacesStore = useWorkspacesStore()
+    if (!isValidWorkspaceKey(workspaceId) || !workspacesStore.workspaceKeys.includes(workspaceId)) {
+      return { name: 'home' }
+    }
+  }
 })
 
 export default router

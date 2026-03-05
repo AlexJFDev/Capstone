a52450e4cac48749737bd489191b59706069a91b
alexjfdev <alexjfuhr@gmail.com>
2026-03-05 13:29:58 -0500

    Added date setting validation to store

--- src/stores/items.ts
+++ src/stores/items.ts
@@ -1,6 +1,7 @@
 import { items as dummyItems } from "@/testing/dummy-items"
 import { constructEmptyItem, validateItemKey, type Item } from "@/types"
 import { validateColor } from "@/utils/colors"
+import { validateRange } from "@/utils/dates"
 import { defineStore } from "pinia"
 import { computed, ref } from "vue"
 
@@ -41,6 +42,14 @@ export const useItemsStore = defineStore('items', () => {
     return getItem(key).color
   }
 
+  function getStartDate(key: string): Date {
+    return getItem(key).startDate
+  }
+
+  function getEndDate(key: string): Date {
+    return getItem(key).endDate
+  }
+
   function addNewItem() {
     const key = `i-${crypto.randomUUID()}`
     const item = constructEmptyItem()
@@ -58,11 +67,25 @@ export const useItemsStore = defineStore('items', () => {
     if (updates.color) {
       validateColor(updates.color)
     }
-    if (updates.startDate) {
-      console.log("TODO: Start date validation")
+    if (updates.startDate && updates.endDate) {
+      // Start and end dates being set
+      validateRange({
+        start: updates.startDate,
+        end: updates.endDate
+      })
     }
-    if (updates.endDate) {
-      console.log("TODO: End date validation")
+    else if (updates.startDate) {
+      // Just start date being set
+      validateRange({
+        start: updates.startDate,
+        end: getEndDate(key)
+      })
+    } else if (updates.endDate) {
+      // Just end date being set
+      validateRange({
+        start: getStartDate(key),
+        end: updates.endDate
+      })
     }
 
     Object.assign(getItem(key), updates)

6509380ab322155070cde6b18d6c16ea0ce549a0
alexjfdev <alexjfuhr@gmail.com>
2026-04-08 16:53:14 -0400

    Renamed import

--- src/components/collections/CollectionsPanel.vue
+++ src/components/collections/CollectionsPanel.vue
@@ -1,7 +1,7 @@
 <!-- Right-side drawer listing all workspace cards sorted with the favorite first, plus a button to create a new workspace. -->
 <script setup lang="ts">
 import { computed } from 'vue'
-import WorkspaceCard from './CollectionCard.vue'
+import CollectionCard from './CollectionCard.vue'
 import { useInterfaceStore } from '@/stores/interface'
 
 const props = defineProps<{
@@ -33,7 +33,7 @@ function addCollection() {
 
     <!-- BODY -->
     <div class="pa-3 d-flex flex-column ga-3">
-      <WorkspaceCard
+      <CollectionCard
         v-for="collectionId in sortedWorkspaceIds"
         :key="collectionId"
         :collection-id="collectionId"
